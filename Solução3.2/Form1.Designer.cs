0b74e55bbc1fc7127ad6169ca442acb016b17174
Alice Nascimento Bentes <alicebentes@hotmail.com>
2023-03-23 20:57:01 -0300

    Adicionar arquivos de projeto.

---
+++ Solução3.2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Solução3._2
+{
+    partial class FormLeituraArquivo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.dgvLeitura = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTotalSemReajuste = new System.Windows.Forms.Label();
+            this.lblTotalComReajuste = new System.Windows.Forms.Label();
+            this.ofdListaFuncionarios = new System.Windows.Forms.OpenFileDialog();
+            this.lblPercentualDoReajuste = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLeitura)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Enabled = false;
+            this.txtArquivo.Location = new System.Drawing.Point(24, 13);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.Size = new System.Drawing.Size(464, 20);
+            this.txtArquivo.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(519, 9);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(44, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "...";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // dgvLeitura
+            // 
+            this.dgvLeitura.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLeitura.Location = new System.Drawing.Point(24, 123);
+            this.dgvLeitura.Name = "dgvLeitura";
+            this.dgvLeitura.Size = new System.Drawing.Size(539, 295);
+            this.dgvLeitura.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(24, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(539, 64);
+            this.panel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 170F));
+            this.tableLayoutPanel1.Controls.Add(this.lblTotalSemReajuste, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblTotalComReajuste, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblPercentualDoReajuste, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(532, 57);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblTotalSemReajuste
+            // 
+            this.lblTotalSemReajuste.AutoSize = true;
+            this.lblTotalSemReajuste.BackColor = System.Drawing.Color.Yellow;
+            this.lblTotalSemReajuste.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTotalSemReajuste.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSemReajuste.Location = new System.Drawing.Point(3, 0);
+            this.lblTotalSemReajuste.Name = "lblTotalSemReajuste";
+            this.lblTotalSemReajuste.Size = new System.Drawing.Size(175, 28);
+            this.lblTotalSemReajuste.TabIndex = 0;
+            this.lblTotalSemReajuste.Text = "Total sem reajuste";
+            this.lblTotalSemReajuste.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalComReajuste
+            // 
+            this.lblTotalComReajuste.AutoSize = true;
+            this.lblTotalComReajuste.BackColor = System.Drawing.Color.Yellow;
+            this.lblTotalComReajuste.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTotalComReajuste.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalComReajuste.Location = new System.Drawing.Point(184, 0);
+            this.lblTotalComReajuste.Name = "lblTotalComReajuste";
+            this.lblTotalComReajuste.Size = new System.Drawing.Size(175, 28);
+            this.lblTotalComReajuste.TabIndex = 1;
+            this.lblTotalComReajuste.Text = "Total Com Reajuste";
+            this.lblTotalComReajuste.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ofdListaFuncionarios
+            // 
+            this.ofdListaFuncionarios.DefaultExt = "*.txt:";
+            this.ofdListaFuncionarios.FileName = "listaFuncionarios";
+            this.ofdListaFuncionarios.Filter = "Arquivos textos|*.txt:";
+            this.ofdListaFuncionarios.Title = "Seleção do arquivo com dados de funcionários";
+            // 
+            // lblPercentualDoReajuste
+            // 
+            this.lblPercentualDoReajuste.AutoSize = true;
+            this.lblPercentualDoReajuste.BackColor = System.Drawing.Color.Yellow;
+            this.lblPercentualDoReajuste.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPercentualDoReajuste.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPercentualDoReajuste.Location = new System.Drawing.Point(365, 0);
+            this.lblPercentualDoReajuste.Name = "lblPercentualDoReajuste";
+            this.lblPercentualDoReajuste.Size = new System.Drawing.Size(164, 28);
+            this.lblPercentualDoReajuste.TabIndex = 2;
+            this.lblPercentualDoReajuste.Text = "Percentual Reajuste";
+            this.lblPercentualDoReajuste.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormLeituraArquivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvLeitura);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtArquivo);
+            this.Name = "FormLeituraArquivo";
+            this.Text = "Reajuste de folha de pagamento";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLeitura)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArquivo;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.DataGridView dgvLeitura;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblTotalSemReajuste;
+        private System.Windows.Forms.OpenFileDialog ofdListaFuncionarios;
+        private System.Windows.Forms.Label lblTotalComReajuste;
+        private System.Windows.Forms.Label lblPercentualDoReajuste;
+    }
+}
+
---
+++ Solução3.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Solução3.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Solução3.2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("9a918bcf-07e3-432a-86fb-225687f41561")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
